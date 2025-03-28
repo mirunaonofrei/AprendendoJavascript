10cafbf493aa6009f3f2ace53fd98a72449c450d
mirunaonofrei <miruna@edu.univali.br>
2025-03-28 15:39:00 -0300

    Estudando Javascript 28_03

---
+++ 28_03/exercicio0.js
@@ -0,0 +1,8 @@
+var vel = 101
+console.log(`A velocidade do seu carro é ${vel} km/h`)
+if (vel > 100) {
+    console.log(`Voce ultrapassou a velocidade permitida, Multado.`)
+}
+else {
+    console.log(`Dirija sempre usando cinto`)
+}
