89fc74717da9003d839fef461b6202f4996923e4
mirunaonofrei <miruna@edu.univali.br>
2025-04-01 15:31:56 -0300

    Exercicios usando if

--- 31_03/exercicio2/script.js
+++ 31_03/exercicio2/script.js
@@ -9,27 +9,36 @@ function vefificaidade() {
         var fsex = document.getElementsByName('radsex')
         var idade = ano - Number(fano.value)
         var genero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
         if (idade < 18) {
             if (fsex[0].checked) {
                 genero = 'Homem'
+                img.setAttribute('src','imagens/masc_crianca.jpg')
             } else {
                 genero = 'Mulher'
+                img.setAttribute('src','imagens/fem_crianca.jpg')
             }
 
         } else if (idade < 60) {
             if (fsex[0].checked) {
                 genero = 'Homem'
+                img.setAttribute('src','imagens/masc_adulto.jpg')
             } else {
                 genero = 'Mulher'
+                img.setAttribute('src','imagens/fem_adulto.jpg')
             }
         } else {
             if (fsex[0].checked) {
                 genero = 'Homem'
+                img.setAttribute('src','imagens/masc_idoso.jpg')
             } else {
                 genero = 'Mulher'
+                img.setAttribute('src','imagens/fem_idoso.jpg')
             }
         }
         resultado.style.textAlign = 'center'
         resultado.innerHTML = `Detectamos ${genero} de ${idade} anos!`
+        resultado.appendChild(img)
     }
 }
